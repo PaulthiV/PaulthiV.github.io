1514cd6f912835c762b4e72e7adf85f731e62baf
PaulthiV <paulthi.victor@gmail.com>
2026-04-22 20:21:43 +0200

    deployment yml

--- Development/German_vocab/vite.config.ts
+++ Development/German_vocab/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/', // For root-level deployment (https://paulthiv.github.io/)
   plugins: [react()],
 });
